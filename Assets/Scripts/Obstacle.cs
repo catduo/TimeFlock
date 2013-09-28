c3c9fd84536152e0cb122f729bdfd408c175e90e
David Geisert <geisert@stanfordalumni.org>
2013-09-28 12:21:01 -0700

    updated scene

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter(Collider other) {
+		if(other.name == "FlockZone(Clone)"){
+			
+		}
+		else{
+			GUIControls.gameOver = true;
+		}
+	}
+}
--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -5,6 +5,8 @@ public class Parallax : MonoBehaviour {
 	
 	public float movementSpeed;
 	private float distanceTravelled;
+	private float rewindSpeed = 20;
+	private int rewindFrames = 0;
 	
 	// Use this for initialization
 	void Start () {
@@ -15,5 +17,12 @@ public class Parallax : MonoBehaviour {
 	void FixedUpdate () {
 		transform.Translate(new Vector3(-movementSpeed, 0, 0));
 		distanceTravelled += movementSpeed;
+	}
+	
+	void Reset () {
+		while(){
+			transform.Translate(new Vector3(movementSpeed * rewindSpeed, 0, 0));
+			rewindFrames++;
+		}
 	}
 }
