756d13bfa3fc924af1a322e7a4172adff69b29fd
Frank Huang <me@nongraphical.com>
2013-09-28 12:15:06 -0700

    Updated controls for replay and rewind

--- Assets/Scripts/controls.cs
+++ Assets/Scripts/controls.cs
@@ -1,52 +1,127 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
+
+public struct BirdRewindState {
+	public float PosX, PosY;
+	public int Alive;
+}
+
+public struct BirdInputState {
+	public int HAxis, VAxis;
+}
+
+public enum BirdState {
+	PlayerControlled,
+	Replaying,
+	Rewinding,
+	Dead
+};
 
 public class controls : MonoBehaviour {
 	
-	public int force = 20;
+	public static Vector3 StartingPosition;
+	
+	BirdState currState;
+	List<BirdInputState> inputs;
+	List<BirdRewindState> rewind;
+	int currFrame;
+	
+	public float force = 20;
+	
+	public void InitState(BirdState s) {
+		currState = s;
+		if (s == BirdState.Dead) {
+			this.enabled = false;
+			this.gameObject.SetActive(false);
+		}
+		else {
+			this.enabled = true;
+			this.gameObject.SetActive(true);
+			
+			if (s == BirdState.PlayerControlled) {
+				inputs.Clear();
+			}
+			
+			if (s == BirdState.PlayerControlled || s == BirdState.Replaying) {
+				currFrame = 0;
+				rewind.Clear();
+				transform.position = StartingPosition;
+				rigidbody.velocity = Vector3.zero;
+			}
+			else if (s == BirdState.Rewinding) {
+				currFrame = rewind.Count-1;
+				var rs = rewind[currFrame];
+				transform.position = new Vector3(rs.PosX, rs.PosY, 0.0f);
+			}
+		}
+	}
 	
 	// Use this for initialization
 	void Start () {
-	
+		currState = BirdState.Dead;
+		inputs = new List<BirdInputState>();
+		rewind = new List<BirdRewindState>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		GetKeys ();
+	void FixedUpdate () {
+		if (currState == BirdState.Dead) {
+			return;
+		}
+		
+		switch (currState) {
+		case BirdState.PlayerControlled:
+			var bis = GetKeys();
+			inputs.Add (bis);
+			ApplyInputs(bis);
+			break;
+		case BirdState.Replaying:
+			DoReplay();
+			break;
+		case BirdState.Rewinding:
+			DoRewind();
+			break;
+		}
 	}
 	
-	void GetKeys () {
+	BirdInputState GetKeys () {
+		var bis = new BirdInputState();
 		if (Input.GetKey (KeyCode.W)){
-			controlUp();
+			bis.VAxis += 1;
 		}
 		if (Input.GetKey (KeyCode.S)){
-			controlDown();
+			bis.VAxis -= 1;
 		}
 		if (Input.GetKey (KeyCode.A)){
-			controlLeft();
+			bis.HAxis -= 1;
 		}
 		if (Input.GetKey (KeyCode.D)){
-			controlRight();
+			bis.HAxis += 1;
+		}
+		return bis;
+	}
+	
+	public void ApplyInputs(BirdInputState bis) {
+		rigidbody.AddForce(bis.HAxis * force, bis.VAxis * force, 0.0f);
+		var brs = new BirdRewindState();
+		brs.PosX = transform.position.x;
+		brs.PosY = transform.position.y;
+		rewind.Add (brs);
+	}
+	
+	void DoReplay() {
+		ApplyInputs(inputs[currFrame]);
+		currFrame += 1;
+		if (currFrame >= inputs.Count) {
+			InitState(BirdState.Dead);
 		}
 	}
 	
-	//press up, keys or touch controls
-	void controlUp(){
-		rigidbody.AddForce(Vector3.up * force);
-	}
-	
-	//press down, keys or touch controls
-	void controlDown(){
-		rigidbody.AddForce(Vector3.down * force);
-	}
-	
-	//press left, keys or touch controls
-	void controlLeft(){
-		rigidbody.AddForce(Vector3.left * force);
-	}
-	
-	//press right, keys or touch controls
-	void controlRight(){
-		rigidbody.AddForce(Vector3.right * force);
+	void DoRewind() {
+		if (currFrame < 0) {
+			return;
+		}
+		transform.position = new Vector3(rewind[currFrame].PosX, rewind[currFrame].PosY, 0.0f);
+		currFrame -= 1;
 	}
 }
