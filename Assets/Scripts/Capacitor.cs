471a19b23b89c079b24ca2694134afba55e32862
Jourdan <jourdang@10ddb1bab56e.ant.amazon.com>
2013-09-28 15:03:21 -0700

    Adding capacitor explosions

---
+++ Assets/Scripts/Capacitor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Capacitor : MonoBehaviour {
+	
+	public float state = 0f;
+	private float maxState = 100f;
+	public bool stateBackwards = false;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		state += (stateBackwards? - 5 : 5);
+		//Debug, we might do whatever animation instead of scale
+		transform.localScale = new Vector3(3*state/100, 3*state/100, 3*state/100);
+		if (state == 100) stateBackwards = true;
+		//if (state == 0) stateBackwards = false;
+	}
+}
--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -16,10 +16,14 @@ public class Obstacle : MonoBehaviour {
 	void OnTriggerEnter(Collider other) {
 		if(other.name == "FlockZone(Clone)"){
 			if (name == "CurrentBird"){
-				float forceX = 200*(transform.position.x - other.transform.position.x);
-				float forceY = 200*(transform.position.y - other.transform.position.y);
-				print ("applying force x="+forceX+" y="+forceY);
-				gameObject.rigidbody.AddForce(new Vector3(forceX, forceY, 0));
+				float obstacleState = other.GetComponent<Capacitor>().state;
+				float forceX = transform.position.x - other.transform.position.x;
+				float forceY = transform.position.y - other.transform.position.y;
+				Vector3 forceVector = new Vector3(forceX, forceY, 0);
+				
+				if (!other.GetComponent<Capacitor>().stateBackwards){
+					gameObject.rigidbody.AddForce((100f+4.5f*(100f-obstacleState))*forceVector.normalized);
+				}
 			}
 		}
 		else{
