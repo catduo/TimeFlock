f88ba87d6b7b2ccd532856645775089944a30ee1
Frank Huang <me@nongraphical.com>
2013-09-29 14:47:46 -0700

    Door rewinding

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,21 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Door : MonoBehaviour {
+public class Door : RewindableObject<float> {
 	public int distance = 15;
 	public int speedX = 0;
 	public int speedY = 5;
 	
 	// Use this for initialization
-	void Start () {
-	
+	override protected void Start () {
+		base.Start();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	override protected void ForwardFixedUpdate () {
 		if (transform.position.x <= distance) {
 			rigidbody.velocity = new Vector3(speedX, speedY, 0);
 		}
+		AddRewindState(transform.position.y, false);
+	}
+	
+	override protected void ApplyCustom(float y) {
+		transform.position = new Vector3(transform.position.x, y, transform.position.z);
 	}
 		
 }
