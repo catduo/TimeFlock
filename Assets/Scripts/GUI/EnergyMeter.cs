96fd3851958b35f0e86563df6cb34148dfd3011d
David Geisert <geisert@stanfordalumni.org>
2013-09-28 17:37:30 -0700

    energy meter

---
+++ Assets/Scripts/GUI/EnergyMeter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnergyMeter : MonoBehaviour {
+	
+	private float width;
+	private float xpos;
+	
+	// Use this for initialization
+	void Start () {
+		xpos = 0;
+		width = 0.9F;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		width = 0.9F * (100 - GUIControls.distance);
+		xpos = width - 0.9F;
+	}
+	
+	void Hold(){
+		//slow time
+	}
+}
